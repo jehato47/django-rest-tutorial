e397ad0576ba63c07c20274f0923ffe4ee618952
jehato47 <jehatdeniz@hotmail.com>
2022-08-21 11:58:15 +0300

    cbasedapi

--- first_app/urls.py
+++ first_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
-from .views import country_list, country_detail
+from .views import country_list, country_detail, CountryView, CountryDetailView
 
 urlpatterns = [
-    path("list/", country_list),
-    path("detail/<int:pk>", country_detail)
+    # path("list/", country_list),
+    # path("detail/<int:pk>", country_detail),
+
+    path("list/", CountryView.as_view()),
+    path("detail/<int:pk>", CountryDetailView.as_view())
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -7,9 +7,58 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 # Create your views here.
+
+
+class CountryView(APIView):
+    def get(self, request):
+        obj = Country.objects.all()
+        serializer = CountrySerializer(obj, many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request):
+        # data = JSONParser().parse(request)
+        serializer = CountrySerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=400)
+
+
+class CountryDetailView(APIView):
+    def get_object(self, id):
+        try:
+            country = Country.objects.get(id=id)
+            return country
+        except Country.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk):
+        country = self.get_object(pk)
+        serializer = CountrySerializer(country)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        country = self.get_object(pk)
+        # data = JSONParser().parse(request)
+        serializer = CountrySerializer(country, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request, pk):
+        country = self.get_object(pk)
+        country.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(["GET", "POST"])
