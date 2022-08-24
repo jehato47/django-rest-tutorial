92fb5849250ded60468c8ae0ba162846236c03cc
jehato47 <jehatdeniz@hotmail.com>
2022-08-24 22:25:03 +0300

    add all

--- first_app/urls.py
+++ first_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from .views import CountryView, CountryDetailView
+from .views import CountryView, CountryDetailView, GenericApiView
 
 urlpatterns = [
     # path("list/", country_list),
     path("list/", CountryView.as_view()),
     # path("detail/<int:pk>", country_detail)
-    path("detail/<int:pk>", CountryDetailView.as_view())
+    path("detail/<int:pk>", CountryDetailView.as_view()),
+    path("generic/<int:id>", GenericApiView.as_view()),
+
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -8,9 +8,34 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework import mixins
 
 
 # Create your views here.
+
+class GenericApiView(
+    generics.GenericAPIView, mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin,
+    mixins.UpdateModelMixin, mixins.DestroyModelMixin,
+):
+    serializer_class = CountrySerializer
+    queryset = Country.objects.all()
+    lookup_field = "id"
+
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+    def delete(self, request, id=None):
+        return self.destroy(request, id)
 
 
 class CountryView(APIView):
